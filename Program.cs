6038b0bbed26a87871cf3d09b9574929da6f793d
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-11-11 23:25:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int[] CountBy(int x, int n)
+    {
+        return Enumerable.Range(1, n).Select(i => i * x).ToArray();
+    }
+}
